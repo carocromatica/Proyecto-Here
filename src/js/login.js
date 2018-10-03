032ed9f041a025c2a959a11c6cb5eff503ae4f11
milelym <milelymarcial@gmail.com>
2018-10-02 23:37:02 -0300

    facebook

--- src/js/login.js
+++ src/js/login.js
@@ -18,3 +18,23 @@ console.log(listo);
   });
 }
 
+function facebook(){
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      location='../html/map.html'
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+    }
