a1bb9482945e2fc074ae0b345f664ab5a048c32e
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-10-03 13:48:14 -0300

    experimentando con avatar y firebase

--- src/js/select.js
+++ src/js/select.js
@@ -3,30 +3,56 @@ firebase.auth().onAuthStateChanged(function (user) {
     let usermail = firebase.auth().currentUser.email;
     document.getElementById("usermail").innerHTML = usermail;
 
-   
+  
+
+
+    firebase.database().ref(`usuarios/${firebase.auth().currentUser.uid}/avatar`)
+    .on("child_added", (avatar) => {
+
+      asd.innerHTML = `
+    
+      <img id="personaje" src='${avatar.val()}' class="responsive-img avatar"> 
+      `     
+      + asd.innerHTML;
+      console.log(avatar)
+
+    });
+
   } else {
     window.location = "index.html";
   }
 });
 
 
-
 function selectElfa(){
   alert('elegiste a elfa')
-  document.getElementById("PERSONAJE").src = "../../assets/img/avatars/elfa.png";
+
+  const mono=document.getElementById("personaje").src = "https://i.imgur.com/IsFXKks.png";
+  const currentUser = firebase.auth().currentUser; // esta indica si estamos logeadas
+  firebase.database().ref(`usuarios/${currentUser.uid}/avatar`).update({
+    mono
+  });
+
+
 }
 
 function selectMago(){
+
   alert('elegiste a mago')
-  document.getElementById("PERSONAJE").src = "../../assets/img/avatars/mago.png";
+  const mono=document.getElementById("personaje").src = "https://i.imgur.com/OD2BpX0.png";
+  const currentUser = firebase.auth().currentUser; // esta indica si estamos logeadas
+  firebase.database().ref(`usuarios/${currentUser.uid}/avatar`).update({
+    mono
+  });
+
 }
 
 function selectRobin(){
   alert('elegiste a robin')
-  document.getElementById("PERSONAJE").src = "../../assets/img/avatars/exportar.png";
+  document.getElementById("personaje").src = "../../assets/img/avatars/exportar.png";
 }
 
 function selectChica(){
   alert('elegiste a chica')
-  document.getElementById("PERSONAJE").src = "../../assets/img/avatars/chicaRuda.png";
+  document.getElementById("personaje").src = "../../assets/img/avatars/chicaRuda.png";
 }
