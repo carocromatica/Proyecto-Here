4a5bd234daf08e40e77a97d8411f79364e951cf6
MLoretto <loreto.henriquez.m@gmail.com>
2018-10-03 09:10:37 -0300

    merge

--- src/js/login.js
+++ src/js/login.js
@@ -15,6 +15,29 @@ console.log(listo);
     var errorMessage = error.message;
     console.log(errorCode)
     console.log(errorMessage)
+    errores.innerHTML= '';
+    errores.innerHTML +=`<p class='red-text'>${"Oops! Contraseña o Email inválido"}</p>`
   });
 }
 
+function facebook(){
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      location='../html/map.html'
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+    }
+    
---
+++ src/js/select.js
@@ -0,0 +1,19 @@
+function selectElfa(){
+  alert('elegiste a elfa')
+  document.getElementById("PERSONAJE").src = "../../assets/img/avatars/elfa.png";
+}
+
+function selectMago(){
+  alert('elegiste a mago')
+  document.getElementById("PERSONAJE").src = "../../assets/img/avatars/mago.png";
+}
+
+function selectRobin(){
+  alert('elegiste a robin')
+  document.getElementById("PERSONAJE").src = "../../assets/img/avatars/exportar.png";
+}
+
+function selectChica(){
+  alert('elegiste a chica')
+  document.getElementById("PERSONAJE").src = "../../assets/img/avatars/chicaRuda.png";
+}
