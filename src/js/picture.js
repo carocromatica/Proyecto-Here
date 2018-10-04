e4eb8f21f0ec75129df1333b65ad485a6d69e724
MLoretto <loreto.henriquez.m@gmail.com>
2018-10-04 02:52:42 -0300

    permisos con facebook otra vez

--- src/js/picture.js
+++ src/js/picture.js
@@ -94,6 +94,66 @@ function SendFB(){
 
 }
 
+function postImageToFacebook(token, filename, mimeType, imageData, message) {
+    var fd = new FormData();
+    fd.append("access_token", token);
+    fd.append("source", imageData);
+    fd.append("no_story", true);
+
+    // Upload image to facebook without story(post to feed)
+    $.ajax({
+        url: "https://graph.facebook.com/me/photos?access_token=" + token,
+        type: "POST",
+        data: fd,
+        processData: false,
+        contentType: false,
+        cache: false,
+        success: function (data) {
+            console.log("success: ", data);
+
+            // Get image source url
+            FB.api(
+                "/" + data.id + "?fields=images",
+                function (response) {
+                    if (response && !response.error) {
+                        //console.log(response.images[0].source);
+
+                        // Create facebook post using image
+                        FB.api(
+                            "/me/feed",
+                            "POST",
+                            {
+                                "message": "",
+                                "picture": response.images[0].source,
+                                "link": window.location.href,
+                                "name": 'Look at the cute panda!',
+                                "description": message,
+                                "privacy": {
+                                    value: 'SELF'
+                                }
+                            },
+                            function (response) {
+                                if (response && !response.error) {
+                                    /* handle the result */
+                                    console.log("Posted story to facebook");
+                                    console.log(response);
+                                }
+                            }
+                        );
+                    }
+                }
+            );
+        },
+        error: function (shr, status, data) {
+            console.log("error " + data + " Status " + shr.status);
+        },
+        complete: function (data) {
+            //console.log('Post to facebook Complete');
+        }
+    });
+}
+
+
     /*
   function subirArchivo(archivo, nombre, key) {
     console.log('Subir Archivo');
