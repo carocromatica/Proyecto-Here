bdffadce9db82949ffa55f479e89be6b0ca831af
MLoretto <loreto.henriquez.m@gmail.com>
2018-10-04 15:26:15 -0300

    Plan B publicación

--- src/js/picture.js
+++ src/js/picture.js
@@ -30,7 +30,6 @@ const constraints = {
       canvas.width = video.videoWidth;
       canvas.height = video.videoHeight;
       canvas.getContext('2d').drawImage(video, 0, 0);
-      // Other browsers will fall back to image/png
       img.src = canvas.toDataURL('image/webp');
       
       video.style.display = 'none';
@@ -59,21 +58,14 @@ const constraints = {
     console.log('Enviando foto');
     return new Blob([u8arr], {type:mime});
   }
-/*
-    function savePhoto(){
-        console.log("boton guardar");
-        let newRegistroKey = firebase.database().ref().child('registros').push().key;
-        subirArchivo(dataURLtoBlob(img.src), newRegistroKey + '.jpg', newRegistroKey);
-        
-    }
-  */  
 
 function SendFB(){
     let idSelecionado = localStorage.getItem("selectId");
     let armaSelecionado = localStorage.getItem("selectArma");
     let qrSelecionado = localStorage.getItem("selectQR");
 
-
+    
+/*
     var blob = dataURLtoBlob(img.src);
     FB.getLoginStatus(function (response) {
         console.log(response);
@@ -89,7 +81,7 @@ function SendFB(){
             }, {scope: "public_profile, publish_pages, manage_pages"});
         }
     });
-
+*/
 }
 
 function postImageToFacebook(token, filename, mimeType, imageData, message) {
@@ -146,40 +138,9 @@ function postImageToFacebook(token, filename, mimeType, imageData, message) {
             console.log("error " + data + " Status " + shr.status);
         },
         complete: function (data) {
-            //console.log('Post to facebook Complete');
         }
     });
 }
 
 
-    /*
-  function subirArchivo(archivo, nombre, key) {
-    console.log('Subir Archivo');
-    console.log(archivo);
-    console.log(nombre);
-    let storageService = firebase.storage();
-    // creo una referencia al lugar donde guardaremos el archivo
-    let refStorage = storageService.ref('userImages').child(nombre);
-    // Comienzo la tarea de upload
-    const uploadTask = refStorage.put(archivo);
-    // defino un evento para saber qué pasa con ese upload iniciado
-    uploadTask.on('state_changed', null,
-        function(error){
-            console.log('Error al subir el archivo', error);
-        },
-        function(){
-            //obtiene la url de la imagen recien subida
-            uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-                //cambia el source de la imagen por la url de la imagen recien subida
-                console.log('Listo, quedo en :' + downloadURL + ' y la key ' + key);
-                register["urlPicture"] = downloadURL;
-                register["key"] = key;
-                localStorage.setItem('register',JSON.stringify(register));
-                console.log(register);
-                window.location = 'firma.html';
-                //saveData(downloadURL,key);
-              });
-        }
-    );
-}
-*/
+ 
